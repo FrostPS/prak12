5175733a36d6b43e7bc7de24d6b8e026d0217960
Nikolay <mr.nikolafet@mail.ru>
2023-04-06 15:04:26 +0300

    test

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,7 +2,7 @@ import requests
 api = 'http://localhost:7000'
 def test_health():
     response =requests.get(f'{api}/health')
-    assert response.status_code == 200
+    assert response.status_code == 400
 def test_bye():
     response =requests.get(f'{api}/fir_bye')
     assert response.status_code == 200
